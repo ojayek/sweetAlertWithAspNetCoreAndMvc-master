c3138e717a74c152f2328fa186384e4caa06110a
ojayek <ghoreyshi@gmail.com>
2020-10-12 11:52:28 +0330

    first commit

---
+++ project/SweetAlertBlog/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using static SweetAlertBlog.Enums.Enums;
+
+namespace SweetAlertBlog.Controllers
+{
+    public class TestController : BaseController
+    {
+        public IActionResult Index()
+        {
+            Alert("ECE SENT", NotificationType.success);
+            return View();
+        }
+    }
+}
